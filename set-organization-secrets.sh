59f657ea6d6ebd7733217c74fb1848fa8c465de5
jesse-grabowski <github@jessegrabowski.com>
2025-11-11 19:30:49 +0900

    Fix organization secret script

--- set-organization-secrets.sh
+++ set-organization-secrets.sh
@@ -22,10 +22,10 @@ TOKEN_ID=$(tofu output -raw terraform_state_token_id)
 TOKEN_VALUE=$(tofu output -raw terraform_state_token_value)
 
 echo "Updating GitHub organization secrets for '$ORG_NAME'..."
-echo "$BUCKET_NAME"    | gh secret set TERRAFORM_STATE_BUCKET_NAME     --org "$ORG_NAME" --visibility all --body -
-echo "$BUCKET_LOCATION"| gh secret set TERRAFORM_STATE_BUCKET_LOCATION --org "$ORG_NAME" --visibility all --body -
-echo "$BUCKET_ENDPOINT"| gh secret set TERRAFORM_STATE_BUCKET_ENDPOINT --org "$ORG_NAME" --visibility all --body -
-echo "$TOKEN_ID"       | gh secret set TERRAFORM_STATE_TOKEN_ID        --org "$ORG_NAME" --visibility all --body -
-echo "$TOKEN_VALUE"    | gh secret set TERRAFORM_STATE_TOKEN_VALUE     --org "$ORG_NAME" --visibility all --body -
+gh secret set TERRAFORM_STATE_BUCKET_NAME     --org "$ORG_NAME" --visibility all --body "$BUCKET_NAME"
+gh secret set TERRAFORM_STATE_BUCKET_LOCATION --org "$ORG_NAME" --visibility all --body "$BUCKET_LOCATION"
+gh secret set TERRAFORM_STATE_BUCKET_ENDPOINT --org "$ORG_NAME" --visibility all --body "$BUCKET_ENDPOINT"
+gh secret set TERRAFORM_STATE_TOKEN_ID        --org "$ORG_NAME" --visibility all --body "$TOKEN_ID"
+gh secret set TERRAFORM_STATE_TOKEN_VALUE     --org "$ORG_NAME" --visibility all --body "$TOKEN_VALUE"
 
 echo "Done."
